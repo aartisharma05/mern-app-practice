1c18625c2f43fd4a229944215b5b2fde057d9bc6
aartisharma05 <aartisharma.official1@gmail.com>
2024-04-16 13:12:38 +0530

    "update"

--- frontend/src/Data.js
+++ frontend/src/Data.js
@@ -8,12 +8,25 @@ const Data = () => {
   const handleRemove = (itemId) => {
     setnData(nData.filter((item) => item.id !== itemId));
   };
+
+  const handleUpdate = (itemId) => {
+    setnData(
+      nData.map((item) => {
+        if (item.id === itemId) {
+          return { name: "newName" };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
   return (
     <div>
       <ul>
         {nData.map((item) => (
           <li key={item.id}>
             {item.name}
+            <button onClick={() => handleUpdate(item.id)}>Update</button>
             <button onClick={() => handleRemove(item.id)}>Remove</button>
           </li>
         ))}
